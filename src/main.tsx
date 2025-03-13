642447327ce52a7eecaccfdc3b22d4380e28afc6
LitteBoyS2CMD <mungnvph52815@gmail.com>
2025-03-13 22:40:35 +0700

    ListProducts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+);
---
+++ src/types/product.tsx
@@ -0,0 +1,9 @@
+export interface IMilktea {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+}
+
+
