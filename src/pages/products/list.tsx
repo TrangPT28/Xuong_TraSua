0d381025fb770cfaa647c3edae3ad3a63273020e
LitteBoyS2CMD <mungnvph52815@gmail.com>
2025-03-14 14:10:21 +0700

    editFile

--- src/pages/products/list.tsx
+++ src/pages/products/list.tsx
@@ -1,14 +1,15 @@
 import { IMilktea } from "../../types/product";
 import axios from "axios";
 import {
-    LaptopOutlined,
-    NotificationOutlined,
-    UserOutlined,
-  } from "@ant-design/icons";
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Breadcrumb, Layout, Menu, Table, theme } from "antd";
-import { useQuery } from "@tanstack/react-query";
+import { Breadcrumb, Button, Layout, Menu, Popconfirm, Table, message, theme } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const { Header, Content, Sider } = Layout;
 
@@ -18,61 +19,94 @@ const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
 }));
 
 const items2: MenuProps["items"] = [
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  ].map((icon, index) => {
-    const key = String(index + 1);
-  
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      children: Array.from({ length: 4 }).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
+
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: Array.from({ length: 4 }).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
 });
 const MilkteaList: React.FC = () => {
-    const {data} = useQuery<IMilktea[]>({
+  const queryClient = useQueryClient();
+  const [messageApi, contextHolder] = message.useMessage();
+  const { mutate } = useMutation({
+    mutationFn: async (id) => {
+      const response = await axios.delete(`http://localhost:3000/milktea/${id}`);
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
         queryKey: ["milktea"],
-        queryFn: async () => {
-            const response = await axios.get("http://localhost:3000/milktea");
-            return response.data;
-        }
-    })
+      });
+      messageApi.success("Completed !");
+    }
+  });
 
-    const columns = [
-        {
-          title: 'ID',
-          dataIndex: 'id',
-          key: 'id'
-        },
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name'
-        },
-        {
-          title: 'Price',
-          dataIndex: 'price',
-          key: 'price'
-        },
-        {
-            title: 'Description',
-            dataIndex: 'description',
-            key: 'description'
-        },
-        {
-            title: 'Image',
-            dataIndex: 'image',
-            key: 'image'
-        },
-      ];
+  const { data } = useQuery<IMilktea[]>({
+    queryKey: ["milktea"],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:3000/milktea");
+      return response.data
+    },
+  });
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+    },
+    {
+      dataIndex: "action",
+      render: (_: any, item: any) => {
+        return (
+          <><Button type="primary">
+            <Link to={`/admin/milkteas/edit/${item.id}`}>Sửa</Link>
+          </Button><Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            onConfirm={() => mutate(item.id)}
+            okText="Yes"
+            cancelText="No"
+          >
+              <Button danger>Delete</Button>
+            </Popconfirm></>
+        );
+      },
+    },
+  ];
 
   const {
     token: { colorBgContainer, borderRadiusLG },
@@ -114,7 +148,8 @@ const MilkteaList: React.FC = () => {
               borderRadius: borderRadiusLG,
             }}
           >
-             <Table dataSource={data} columns={columns} rowKey="id" />
+            <Table dataSource={data} columns={columns} rowKey="id" />
+            {contextHolder}
           </Content>
         </Layout>
       </Layout>
