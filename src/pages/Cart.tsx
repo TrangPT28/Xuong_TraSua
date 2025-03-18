40b8714e64634921de46f6c6eaca9ced4103a815
trangpt <trangptph53066@gmail.com>
2025-03-18 15:31:15 +0700

    update

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,62 +3,6 @@ import "remixicon/fonts/remixicon.css";
 const Cart = () => {
   return (
     <>
-      <header className="flex justify-between items-center px-8 py-4 shadow-md bg-white">
-        {/* Logo */}
-        <div className="flex items-center space-x-2">
-          <span className="text-yellow-700 text-2xl font-bold">&#x1F54A;</span>
-          <span className="text-xl font-semibold">Furniro</span>
-        </div>
-
-        {/* Navigation */}
-        <nav>
-          <ul className="flex space-x-8 text-gray-700">
-            <li>
-              <a href="#" className="hover:text-black">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-black">
-                Shop
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-black">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-black">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Icons */}
-        <div className="flex space-x-6 text-gray-700">
-          <a href="#" className="hover:text-black">
-            &#128100;
-          </a>
-          <a href="#" className="hover:text-black">
-            &#128269;
-          </a>
-          <a href="#" className="hover:text-black">
-            &#10084;
-          </a>
-          <a href="#" className="hover:text-black">
-            &#128717;
-          </a>
-        </div>
-      </header>
-
-      {/* Banner */}
-      <div className="bg-gray-100 text-center h-[318px] flex flex-col justify-center">
-        <h1 className="text-3xl font-bold">Cart</h1>
-        <p className="text-gray-500">Home &gt; Cart</p>
-      </div>
-
       <div className="max-w-7xl mx-auto p-7">
         {/* Cart Table & Cart Totals Container */}
         <div className="mt-8 flex flex-col lg:flex-row gap-6 items-start">
@@ -178,62 +122,6 @@ const Cart = () => {
           </div>
         </div>
       </div>
-
-      {/* Footer */}
-      <footer className="bg-white w-full py-12 border-t border-gray-300">
-        <div className="max-w-6xl mx-auto px-6 grid grid-cols-4 gap-8">
-          {/* Logo + Address */}
-          <div>
-            <h2 className="text-2xl font-bold">Funiro.</h2>
-            <p className="text-gray-500 mt-2">
-              400 University Drive Suite 200 Coral Gables,
-              <br />
-              FL 33134 USA
-            </p>
-          </div>
-
-          {/* Links */}
-          <div>
-            <h3 className="text-gray-400 font-semibold">Links</h3>
-            <ul className="mt-2 space-y-2 py-2">
-              <li className="text-black py-2">Home</li>
-              <li className="text-black py-2">Shop</li>
-              <li className="text-black py-2">About</li>
-              <li className="text-black py-2">Contact</li>
-            </ul>
-          </div>
-
-          {/* Help */}
-          <div>
-            <h3 className="text-gray-400 font-semibold">Help</h3>
-            <ul className="mt-2 space-y-2 py-2">
-              <li className="text-black py-2">Payment Options</li>
-              <li className="text-black py-2">Returns</li>
-              <li className="text-black py-2">Privacy Policies</li>
-            </ul>
-          </div>
-
-          {/* Newsletter */}
-          <div>
-            <h3 className="text-gray-400 font-semibold">Newsletter</h3>
-            <div className="mt-2 flex  border-gray-400 gap-4">
-              <input
-                type="email"
-                placeholder="Enter Your Email Address"
-                className="flex-1 bg-transparent outline-none text-gray-600 border-b"
-              />
-              <button className="text-black font-semibold border-b">
-                SUBSCRIBE
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {/* Bottom Copyright */}
-        <div className="border-t border-gray-300 mt-6 pt-4 text-center text-gray-500">
-          2023 funiro. All rights reserved
-        </div>
-      </footer>
     </>
   );
 };
